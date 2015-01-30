5a5fc996c9a7f8e51281e2ae9cc12c1f089d4651
bl4ckdu5t <joerexko@gmail.com>
2015-01-30 17:36:58 -0600

    Included the s3 argument for the user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :role, :password, :avatar, :firstname, :lastname, :current)
+    params.require(:user).permit(:email, :role, :password, :avatar, :name, :current)
   end
   def init
     @preferences = Preference.find(1)
--- app/models/staff.rb
+++ app/models/staff.rb
@@ -3,7 +3,7 @@ class Staff < ActiveRecord::Base
 		thumb: '100x100>',
 		square: '200x200#',
 		medium: '300x300>'
-	}, default_url: ':attachment/missing.png'
+	}, default_url: ':attachment/missing.png', storage: :s3
 	# Validate the attached image is image/jpg, image/png, etc
 	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
